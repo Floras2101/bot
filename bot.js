f68caf4c7623eefcc2a7a5b05b93fa85d66a8e7c
Floras2101 <49788701+Floras2101@users.noreply.github.com>
2019-04-19 23:04:06 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,1 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-client.on('ready', () => {
-  console.log('I am ready!');
-});
 
-client.on('message', message => {
-  if (message.content === '^Demare') {
-    message.channel.send('Fapie sobie');
-  }
-});
-client.login('NTY4Nzg1OTA1MDA3OTg0NjQx.XLojmg.EHdG2WiLNHgKXZXQvGL1dpoXK7A');
