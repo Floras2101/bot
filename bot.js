9111bca83f3222717e26d49ae2eecf45df6da469
Floras2101 <49788701+Floras2101@users.noreply.github.com>
2019-04-20 00:19:33 +0200

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,1 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+client.on('message', message => {
+    if (message.content === 'what is my avatar') {
+        message.reply(message.author.avatarURL);
+    }
+});
+client.login('NTY4Nzg1OTA1MDA3OTg0NjQx.XLpJAw.aBGtTd2t_nE6SGQafZ2pMFHvD7s');
+
 
