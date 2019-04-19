7c82304f06b69d2ad6ac3c921d49f7f6cf62d3f4
Floras2101 <49788701+Floras2101@users.noreply.github.com>
2019-04-19 21:35:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,29 @@
-onst Discord = require("discord.js");
-const auth = require("./auth.json");
+/**
+ * Send a user a link to their avatar
+ */
+
+// Import the discord.js module
+const Discord = require('discord.js');
+
+// Create an instance of a Discord client
 const client = new Discord.Client();
-const Words = ["9gag"];
-client.on("ready", () => {
- console.log("I am ready!");
+
+/**
+ * The ready event is vital, it means that only _after_ this will your bot start reacting to information
+ * received from Discord
+ */
+client.on('ready', () => {
+  console.log('I am ready!');
 });
 
-client.on("message", (message) => {
- if (Words.some(word => message.content.includes(word))) {
- message.reply("Go away!");
- message.author.send("ONLY PICTURES.");
- message.delete();
- }
+// Create an event listener for messages
+client.on('message', message => {
+  // If the message is "what is my avatar"
+  if (message.content === 'what is my avatar') {
+    // Send the user's avatar URL
+    message.reply(message.author.avatarURL);
+  }
 });
 
-client.login(process.env.BOT_TOKEN);
+// Log our bot in using the token from https://discordapp.com/developers/applications/me
+client.login('your token here');
