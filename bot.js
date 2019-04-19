a8d3ea53430648dafba9672c05ca85e04e2fd41b
Floras2101 <49788701+Floras2101@users.noreply.github.com>
2019-04-19 16:39:38 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,17 @@
+onst Discord = require("discord.js");
+const auth = require("./auth.json");
+const client = new Discord.Client();
+const Words = ["9gag"];
+client.on("ready", () => {
+ console.log("I am ready!");
+});
+
+client.on("message", (message) => {
+ if (Words.some(word => message.content.includes(word))) {
+ message.reply("Go away!");
+ message.author.send("ONLY PICTURES.");
+ message.delete();
+ }
+});
+
+client.login(process.env.BOT_TOKEN);
