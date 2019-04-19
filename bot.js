fd7bb2e46c06023dd07964069bb5b0a375545235
Floras2101 <49788701+Floras2101@users.noreply.github.com>
2019-04-19 22:46:31 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,39 +19,9 @@ client.on('ready', () => {
 // Create an event listener for messages
 client.on('message', message => {
   // If the message is "ping"
-  if (message.content === '^ping') {
+  if (message.content === '^Demare') {
     // Send "pong" to the same channel
-    message.channel.send('o ty kurwo');
+    message.channel.send('Fapie sobie');
   }
 });
-
-// Log our bot in using the token from https://discordapp.com/developers/applications/me
-/**
- * Send a user a link to their avatar
- */
-
-// Import the discord.js module
-const Discord = require('discord.js');
-
-// Create an instance of a Discord client
-const client = new Discord.Client();
-
-/**
- * The ready event is vital, it means that only _after_ this will your bot start reacting to information
- * received from Discord
- */
-client.on('ready', () => {
-  console.log('I am ready!');
-});
-
-// Create an event listener for messages
-client.on('message', message => {
-  // If the message is "what is my avatar"
-  if (message.content === '^avatar') {
-    // Send the user's avatar URL
-    message.reply(message.author.avatarURL);
-  }
-});
-
 client.login('NTY4Nzg1OTA1MDA3OTg0NjQx.XLojmg.EHdG2WiLNHgKXZXQvGL1dpoXK7A');
-
