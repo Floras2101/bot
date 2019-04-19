d6c7a1baad870d0fc6d442b12684410fdfdc00f9
Floras2101 <49788701+Floras2101@users.noreply.github.com>
2019-04-20 01:15:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,58 +1,31 @@
-/**
- * A ping pong bot, whenever you send "ping", it replies "pong".
- */
-
-// Import the discord.js module
 const Discord = require('discord.js');
 
-// Create an instance of a Discord client
 const client = new Discord.Client();
 
-/**
- * The ready event is vital, it means that only _after_ this will your bot start reacting to information
- * received from Discord
- */
 client.on('ready', () => {
   console.log('I am ready!');
 });
 
-// Create an event listener for messages
 client.on('message', message => {
-  // If the message is "ping"
   if (message.content === 'ping') {
-    // Send "pong" to the same channel
     message.channel.send('pong');
   }
 });
 
-// Log our bot in using the token from https://discordapp.com/developers/applications/me
 client.login('/**
- * A ping pong bot, whenever you send "ping", it replies "pong".
- */
-
-// Import the discord.js module
 const Discord = require('discord.js');
 
-// Create an instance of a Discord client
 const client = new Discord.Client();
 
-/**
- * The ready event is vital, it means that only _after_ this will your bot start reacting to information
- * received from Discord
- */
 client.on('ready', () => {
   console.log('I am ready!');
 });
 
-// Create an event listener for messages
 client.on('message', message => {
-  // If the message is "ping"
   if (message.content === 'ping') {
-    // Send "pong" to the same channel
     message.channel.send('pong');
   }
 });
 
-// Log our bot in using the token from https://discordapp.com/developers/applications/me
 client.login(process.env.NTY4Nzg1OTA1MDA3OTg0NjQx.XLpJAw.aBGtTd2t_nE6SGQafZ2pMFHvD7s);
 ');
