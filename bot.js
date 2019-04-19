b8f2ce8b597673c39162f9319ff4acdfbd65fabf
Floras2101 <49788701+Floras2101@users.noreply.github.com>
2019-04-20 01:17:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,31 +1,26 @@
-const Discord = require('discord.js');
+// Extract the required classes from the discord.js module
+const { Client, Attachment } = require('discord.js');
 
-const client = new Discord.Client();
+// Create an instance of a Discord client
+const client = new Client();
 
+/**
+ * The ready event is vital, it means that only _after_ this will your bot start reacting to information
+ * received from Discord
+ */
 client.on('ready', () => {
   console.log('I am ready!');
 });
 
 client.on('message', message => {
-  if (message.content === 'ping') {
-    message.channel.send('pong');
-  }
+    // If the message is '!rip'
+    if (message.content === '!rip') {
+        // Create the attachment using Attachment
+        const attachment = new Attachment('https://i.imgur.com/w3duR07.png');
+        // Send the attachment in the message channel
+        message.channel.send(attachment);
+    }
 });
 
-client.login('/**
-const Discord = require('discord.js');
-
-const client = new Discord.Client();
-
-client.on('ready', () => {
-  console.log('I am ready!');
-});
-
-client.on('message', message => {
-  if (message.content === 'ping') {
-    message.channel.send('pong');
-  }
-});
-
-client.login(process.env.NTY4Nzg1OTA1MDA3OTg0NjQx.XLpJAw.aBGtTd2t_nE6SGQafZ2pMFHvD7s);
-');
+// Log our bot in using the token from https://discordapp.com/developers/applications/me
+client.login('process.env.NTY4Nzg1OTA1MDA3OTg0NjQx.XLpJAw.aBGtTd2t_nE6SGQafZ2pMFHvD7s');
